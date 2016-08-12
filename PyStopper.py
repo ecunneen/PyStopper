a6b6c2fd059a86d3615262d3f4dc9f528831bab6
edwardcunneen <ecunneen@asu.edu>
2016-08-11 17:50:04 -0700

    Feature Complete.
    Added sliders.

--- PyStopper.py
+++ PyStopper.py
@@ -15,7 +15,8 @@ class TopWindow:
         self.lightarea = Canvas(self.root, width=600, height=400)
         self.lightarea.pack()
 
-        self.isGreen = True
+        self.is_green = True
+        self.is_playing = False
 
     # Draws green square. Draws red square.
     def light_green(self):
@@ -39,30 +40,57 @@ class TopWindow:
 
     def toggle_playing(self):
         self.lightarea.delete("all")
-        if self.isGreen:
-            self.isGreen = False
+        if self.is_green:
+            self.is_green = False
             self.light_red()
         else:
-            self.isGreen = True
+            self.is_green = True
             self.light_green()
 
     # Generates one "cycle" of green and red lights
-    def play_cycle(self):
-        self.toggle_playing()
-        self.root.after(random.randint(1000, 5000), self.play_cycle)
+    def play_single_cycle(self):
+        if self.is_playing:
+            self.toggle_playing()
+            rand_max = (self.green_time_scale.get() if self.is_green else self.red_time_scale.get()) * 1000 #convert to ms
+            if rand_max < 1000:
+                rand_max = 1000
+            self.root.after(random.randint(1000, rand_max), self.play_single_cycle)
+
+    def start_playing(self):
+        self.is_playing = True
+        self.play_single_cycle()
+
+    def stop_playing(self):
+        self.is_playing = False
 
     # Code for the exit button.
     def close_program(self):
         self.root.destroy()
 
     def run(self):
-        stopb = Button(self.root, text="Close program", command=self.close_program)
-        stopb.pack(padx=20, pady=20)
+        pad_kwargs = {"padx": 10, "pady": 10}
+        # stopb = Button(self.root, text="Close program", command=self.close_program)
+        # stopb.pack(**pad_kwargs)
 
-        start_button = Button(self.root, text="Start", command=self.play_cycle)
-        start_button.pack(padx=20, pady=20)
+        start_button = Button(self.root, text="Start", command=self.start_playing)
+        start_button.pack(**pad_kwargs)
+
+        stop_button = Button(self.root, text="Stop", command=self.stop_playing)
+        stop_button.pack(**pad_kwargs)
 
         self.light_green()
+
+        self.green_time_scale_lable = Label(text="Time Performing (sec)")
+        self.green_time_scale_lable.pack()
+        self.green_time_scale = Scale(self.root, to=30, orient=HORIZONTAL)
+        self.green_time_scale.set(15)
+        self.green_time_scale.pack()
+
+        self.red_time_scale_lable = Label(text="Time Frozen (sec)")
+        self.red_time_scale_lable.pack()
+        self.red_time_scale = Scale(self.root, to=10, orient=HORIZONTAL)
+        self.red_time_scale.set(2)
+        self.red_time_scale.pack()
         self.root.mainloop()
 
 our_window = TopWindow()
