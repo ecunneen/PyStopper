0176225b00588ce0eeea8e8f9c3759da37348e8d
edwardcunneen <ecunneen@asu.edu>
2016-08-11 21:59:17 -0700

    Reworked user interfact for clarity and usability.

--- PyStopper.py
+++ PyStopper.py
@@ -1,43 +1,48 @@
 import random
-from tkinter import Tk, Frame, Canvas, Button, Scale, Label, HORIZONTAL
+from tkinter import Tk, Frame, Canvas, Button, Scale, Label, HORIZONTAL, E, S
 
 
 # Starts the tkinter loop thingy and gives it a window to do stuff in.
 class TopWindow:
 
     def __init__(self):
+
+        pad_kwargs = {"padx": 0,
+                      "pady": 0,
+                      "ipadx": 0,
+                      "ipady": 0}
+
         self.root = Tk()
         self.frame = Frame(self.root)
-        self.frame.pack()
 
         # Creates canvas on which red/green will be drawn
         self.lightarea = Canvas(self.root, width=600, height=400)
-        self.lightarea.pack()
 
         self.is_green = True
         self.is_playing = False
 
-        pad_kwargs = {"padx": 10, "pady": 10}
-
         self.start_button = Button(self.root, text="Start", command=self.start_playing)
-        self.start_button.pack(**pad_kwargs)
 
         self.stop_button = Button(self.root, text="Stop", command=self.stop_playing)
-        self.stop_button.pack(**pad_kwargs)
 
         self.light_green()
 
         self.green_time_scale_lable = Label(text="Time Performing (sec)")
-        self.green_time_scale_lable.pack()
         self.green_time_scale = Scale(self.root, from_=1, to=30, orient=HORIZONTAL)
         self.green_time_scale.set(15)
-        self.green_time_scale.pack()
 
         self.red_time_scale_lable = Label(text="Time Frozen (sec)")
-        self.red_time_scale_lable.pack()
         self.red_time_scale = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
         self.red_time_scale.set(2)
-        self.red_time_scale.pack()
+
+        self.frame.grid()
+        self.lightarea.grid(row=0, column=0, columnspan=4, **pad_kwargs)
+        self.start_button.grid(row=1, column=0, **pad_kwargs)
+        self.stop_button.grid(row=2, column=0, **pad_kwargs)
+        self.green_time_scale_lable.grid(row=1, column=2, sticky= E + S, **pad_kwargs)
+        self.green_time_scale.grid(row=1, column=3, **pad_kwargs)
+        self.red_time_scale_lable.grid(row=2, column=2, sticky= E + S, **pad_kwargs)
+        self.red_time_scale.grid(row=2, column=3, **pad_kwargs)
 
     # Draws green square. Draws red square.
     def light_green(self):
